c368beaa6885f09785db566d3700873cfc8052cc
NatiGK <natnaelgashu2022@gmail.com>
2023-06-09 11:27:10 +0300

    add payment integration

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ bot.command('start',ctx=>{
     bot.telegram.sendMessage(ctx.chat.id,'hello there! Welcome to my new telegram bot.',{})
 });
 
+bot.command('pay',ctx=>{
+    console.log(ctx.from)
+    bot.telegram.sendInvoice(ctx.chat.id,{
+        title:"Pay",
+        description:"This is a test for paying in tgbot through chapa",
+        payload:"45676",
+        provider_token:"6141645565:TEST:6OCsr8nZ6bUi3qWCP9gt",
+        currency:"ETB",
+        prices:[{label:"Subscription Fee",amount:450}]
+    })
+});
+
 bot.hears('animals',ctx=>{
     console.log(ctx.from);
     let animalMessage = `great, here are pictures of animals you would love`;
